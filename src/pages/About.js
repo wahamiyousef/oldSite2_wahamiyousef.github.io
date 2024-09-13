2d34820eb85589669a09e0c6352f4cab970a281d
Yousef Al-Wahami <alwahami.yousef@gmail.com>
2024-09-12 23:51:29 -0400

    updated about.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,18 @@ function About() {
   return (
     <div id='about' className='aboutContainer'>
       <h1>About Me</h1>
+      <div className='aboutText'>
+        <p>I am currently a student at the University of Windsor studying Computer Science.</p>
+        <p>Here are some technologies I work with:</p>
+        <ul className='techStack'>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>Node.js</li>
+          <li>Python</li>
+          <li>Java</li>
+          <li>C#</li>
+        </ul>
+      </div>
     </div>
   )
 }
